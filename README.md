d301ff5df401e54c3b539f1b8d0ecd46b98f98c2
Noon van der Silk <noonsilk+-noonsilk@gmail.com>
2026-01-18 20:49:03 +0000

    Initial simple python

---
+++ README.md
@@ -0,0 +1,23 @@
+# nix-python-simple
+
+## What's here
+
+A very simple python environment, with no package management. You can see in
+the [flake.nix](./flake.nix) file where to specify python dependecies.
+
+## What's possible
+
+### Hacking on python with the libraries available
+
+``` sh
+python
+...
+>>> import pandas
+```
+
+or
+
+``` sh
+jupyter notebook
+...
+```
---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs: with inputs;
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            (python314.withPackages (ps: with ps; [
+              jupyter
+              numpy
+              pandas
+            ]))
+          ];
+        };
+      }
+    );
+}
